baebf67fca3de92b231b0f8eb6b9e0c7eafa8474
Igor Montagner <igordsm@gmail.com>
2022-02-11 13:59:14 -0300

    Requested changes

--- src/pytest_devlife/settings.py
+++ src/pytest_devlife/settings.py
@@ -5,19 +5,15 @@ import json
 
 def _load_settings_from_disk():
     try:
-        settings_file = osp.join(Path.home(), 'DevLife', '.vscode', 'settings.json')
+        settings_file = Path.home() / 'DevLife' / '.vscode' / 'settings.json'
         with open(settings_file) as f:
             settings = json.load(f)
-            if 'insper.token' in settings and \
-            'insper.hostname' in settings:
-                return {
-                    'token': settings['insper.token'],
-                    'hostname': settings['insper.hostname']
-                }
+            return {
+                'token': settings['insper.token'],
+                'hostname': settings['insper.hostname']
+            }
     except:
-        pass
-
-    raise RuntimeError()
+        raise RuntimeError()
 
 def load_settings():
     token = os.getenv('AUTH_TOKEN')
