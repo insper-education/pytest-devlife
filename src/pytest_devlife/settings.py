2397d7bb30696c90b44a96c851e3ce3c8b3c16ad
Igor Montagner <igordsm@gmail.com>
2022-02-11 15:06:36 -0300

    Load token from DevLife folder if Env Vars not present (#25)
    * Load token from DevLife folder if Env Vars not present
    
    * Requested changes

--- src/pytest_devlife/settings.py
+++ src/pytest_devlife/settings.py
@@ -1,12 +1,25 @@
 from pathlib import Path
 import os
+import os.path as osp
+import json
 
+def _load_settings_from_disk():
+    try:
+        settings_file = Path.home() / 'DevLife' / '.vscode' / 'settings.json'
+        with open(settings_file) as f:
+            settings = json.load(f)
+            return {
+                'token': settings['insper.token'],
+                'hostname': settings['insper.hostname']
+            }
+    except:
+        raise RuntimeError()
 
 def load_settings():
     token = os.getenv('AUTH_TOKEN')
     hostname = os.getenv('AUTH_HOSTNAME')
     if not token or not hostname:
-        raise RuntimeError()
+        return _load_settings_from_disk()
     return {
         'token': token,
         'hostname': hostname
