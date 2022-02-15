5274b36805024bb9297619336cdeffd06d6fd3d8
Igor Montagner <igordsm@gmail.com>
2022-02-15 15:00:56 -0300

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="pytest-devlife",
-    version="0.1.1",
+    version="0.1.2",
     author="Andrew Kurauchi",
     author_email="andrewTNK@insper.edu.br",
     description="Pytest plugin for Insper Developer Life",
